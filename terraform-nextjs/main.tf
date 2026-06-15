d1c509aaa99a78dac44a25fe479e7f156e46f349
ericcoufal <ecoufalleano@gmail.com>
2026-06-14 22:52:44 -0600

    Add private S3 bucket with public access block

--- terraform-nextjs/main.tf
+++ terraform-nextjs/main.tf
@@ -12,3 +12,16 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "website" {
+    bucket = "ericcoufal-portfolio-site"
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+    bucket = aws_s3_bucket.website.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
